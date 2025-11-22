69f7ef744d4331d42cabe8eaae24d078d394385f
Elena <elena_golubkova1982@mail.ru>
2025-11-22 22:37:35 +0400

    Main

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,7 @@
 public class Main {
     public static void main(String[] args) {
         int initialBalance = 100;
-        int topUp = 1600;
+        int topUp = 600;
         int bonus = 0;
 
         if (topUp > 1000) {
