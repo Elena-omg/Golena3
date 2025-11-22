0198982c80724fce24d6c0390e737924c695b3b7
Elena <elena_golubkova1982@mail.ru>
2025-11-22 17:40:28 +0400

    Main

---
+++ src/main.java
@@ -0,0 +1,20 @@
+public class main {
+    public static void main(String[] args) {
+        int initialBalance = 100;
+        int topUp = 600;
+        int bonus = 0;
+
+        if (topUp > 1000) {bonus = (topUp / 100);}
+
+        int finalBalance = initialBalance + topUp + bonus;
+
+        System.out.println("Начальный баланс: " + initialBalance + "руб.");
+        System.out.println("Сумма пополнения: " + topUp + "руб.");
+        System.out.println("Бонусные рубли: " + bonus + "руб.");
+        System.out.println("Итоговый баланс: " + finalBalance + "руб.");
+
+
+
+    }
+}
+
